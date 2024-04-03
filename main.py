0cf489b3e9b301bd7bc6bc3895b3290ffd7a3352
Black1Matter <blackmatter19.70@gmail.com>
2024-04-03 22:06:01 +0300

    for cron

--- main.py
+++ main.py
@@ -6,20 +6,18 @@ from tg_bot.telegram_bot import send_message
 
 def main():
     log, config, setting, wait = configuration_settings()
-    while True:
 
-        data = check_provider_status(log, config, setting)
+    data = check_provider_status(log, config, setting)
 
-        if data != {}:
-            for moniker in data:
-                message = create_message(log, config, data[moniker], moniker)
-                send_message(log, config, message)
-                time.sleep(1)
-        # a = terminal(log, config, 'ls')
+    if data != {}:
+        for moniker in data:
+            message = create_message(log, config, data[moniker], moniker)
+            send_message(log, config, message)
+            time.sleep(1)
+    # a = terminal(log, config, 'ls')
 
-        update_settings(setting)
-        log.info(f"wait {wait} min")
-        time.sleep(wait * 60)
+    update_settings(setting)
+
 
 if __name__ == '__main__':
     main()
